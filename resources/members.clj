1312aa158dec31f989670b39ed81ad81f1cc4915
Seajure <seajure@googlegroups.com>
2010-12-02 21:00:03 -0800

    New members!

--- resources/members.clj
+++ resources/members.clj
@@ -1,4 +1,7 @@
 [{:name "Phil Hagelberg" :url "http://technomancy.us"}
  {:name "Brendan Ribera" :url "http://threebrothers.org/brendan/"}
  {:name "Eric Hanchrow"}
- {:name "Kevin Downey"}]
+ {:name "Kevin Downey"}
+ {:name "Russell Branca" :url "http://chewbranca.com"}
+ {:name "Chris Bilson"}
+ {:name "Stan Dyck" :url "http://sgd.homelinux.net"}]
