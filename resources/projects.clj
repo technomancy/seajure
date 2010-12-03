d39585babd4672a45dbf523ee168d5cb19b55bde
Phil Hagelberg <technomancy@gmail.com>
2010-12-02 21:09:23 -0800

    Switch to https github URLs.

--- resources/projects.clj
+++ resources/projects.clj
@@ -1,88 +1,89 @@
 {:leiningen {:name "Leiningen"
-             :url "http://github.com/technomancy/leiningen"
+             :url "https://github.com/technomancy/leiningen"
              :description "A build tool for Clojure designed not to set your hair on fire."}
 
  :clojure-mode {:name "Clojure Mode"
-                :url "http://github.com/technomancy/clojure-mode"
+                :url "https://github.com/technomancy/clojure-mode"
                 :description "Emacs support for editing Clojure files."}
 
  :swank-clojure {:name "Swank Clojure"
-                 :url "http://github.com/technomancy/swank-clojure"
+                 :url "https://github.com/technomancy/swank-clojure"
                  :description "A SLIME adapter for communicating with Clojure subprocesses from Emacs."}
 
  :clojure-gem {:name "Clojure Gem"
-               :url "http://github.com/technomancy/clojure-gem"
+               :url "https://github.com/technomancy/clojure-gem"
                :description "An adapter for using Clojure's immutable data structures and software transactional memory from JRuby."}
 
  :clojure-http-client {:name "Clojure HTTP Client"
-                       :url "http://github.com/technomancy/clojure-http-client"
+                       :url "https://github.com/technomancy/clojure-http-client"
                        :description "What it says on the box."}
  :mire {:name "Mire"
-        :url "http://github.com/technomancy/mire"
+        :url "https://github.com/technomancy/mire"
         :description "A multiplayer text adventure and learning project."}
 
  :clj-kdtree {:name "clj-kdtree"
-              :url "http://github.com/abscondment/clj-kdtree"
+              :url "https://github.com/abscondment/clj-kdtree"
               :description "kd-trees in Clojure."}
 
  :clojurebot {:name "clojurebot"
-              :url "http://github.com/hiredman/clojurebot"
+              :url "https://github.com/hiredman/clojurebot"
               :description "irc bot for #clojure"}
 
  :Repl {:name "Repl"
-        :url "http://github.com/hiredman/Repl"
+        :url "https://github.com/hiredman/Repl"
         :description "Gui repl"}
 
  :clojure-dependency-grapher
  {:name "clojure-dependency-grapher"
-  :url "http://github.com/hiredman/clojure-dependency-grapher"
+  :url "https://github.com/hiredman/clojure-dependency-grapher"
   :description "generate dot (graphviz) files describing dependencies"}
 
  :serializable-fn
  {:name "Serializable Fn"
-  :url "http://github.com/technomancy/serializable-fn"
+  :url "https://github.com/technomancy/serializable-fn"
   :description "Make your functions print prettily!"}
 
  :robert-hooke
  {:name "Robert Hooke"
-  :url "http://github.com/technomancy/robert-hooke"
+  :url "https://github.com/technomancy/robert-hooke"
   :description "A generalized function extensibility mechanism for plugins."}
 
  :amontillado
  {:name "Amontillado"
-  :url "http://github.com/hiredman/Amontillado"
+  :url "https://github.com/hiredman/Amontillado"
   :description "bitcask inspired storage for clojure"}
 
  :cellar
  {:name "cellar"
-  :url "http://github.com/hiredman/cellar"
+  :url "https://github.com/hiredman/cellar"
   :description "an abstraction over key value stores"}
 
  :Arkham
  {:name "Arkham"
-  :url "http://github.com/hiredman/Arkham"
+  :url "https://github.com/hiredman/Arkham"
   :description "a lisp interpreter"}
 
  :graft
  {:name "graft"
-  :url "http://github.com/hiredman/graft"
+  :url "https://github.com/hiredman/graft"
   :description "translate uris to functions for ring"}
 
  :chinese-democracy
  {:name "Chinese Democracy"
-  :url "http://github.com/hiredman/chinese-democracy"
+  :url "https://github.com/hiredman/chinese-democracy"
   :description "Master^H^H^H^H^H^HChairman Election"}
 
  :cabeza-de-vaca
  {:name "Cabeza de Vaca"
-  :url "http://github.com/Seajure/cabeza-de-vaca"
+  :url "https://github.com/Seajure/cabeza-de-vaca"
   :description "Embark on a voyage of discovery… Local network discovery."}
+
  :radagast
  {:name "Radagast"
-  :url "http://github.com/Seajure/radagast"
+  :url "https://github.com/Seajure/radagast"
   :description "Simplistic test coverage"}
 
  :reducate
  {:name "Reducate"
-  :url "http://github.com/Seajure/reducate"
+  :url "https://github.com/Seajure/reducate"
   :description "A reduce-like macro."}}
